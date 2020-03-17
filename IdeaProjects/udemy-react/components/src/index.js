eedc18a0376e5e7dfd097570e0cfe4fb923805c4
Roger Mayer <rmayer1984@gmail.com>
2020-03-17 13:07:22 -0500

    first commit

--- IdeaProjects/udemy-react/components/src/index.js
+++ IdeaProjects/udemy-react/components/src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom';
 
 const App = () =>{
     return (
-      <div className="ui container comments">
-          <div className="comment">
-              <a href="/" className="avatar">
-                  <img alt="avatar"/>
-              </a>
-              <div className="content">
-                  <a href="/" className="author">
-                      Roger
-                  </a>
-              <div className="metadata">
-                  <span className="date">Today at 6:00pm</span>
-              </div>
-                  <div className="text">Nice Blog Post!</div>
-              </div>
-          </div>
-      </div>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Roger
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">Nice Blog Post!</div>
+                </div>
+            </div>
+        </div>
     );
 };
 
